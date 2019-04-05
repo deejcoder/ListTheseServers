bb9bd69d204dc3afcd1c306fe64db99f88b2a0dd
Dylan Tonks <deejtonks@gmail.com>
2019-04-05 13:42:24 +1300

    not sure why some wasn't pushed earlier, restructure app factory

--- run.py
+++
@@ -1,9 +0,0 @@
-"""
-Simply runs the Flask app, pointing to the sub-package
-"""
-
-from nitroxserverlist import app
-
-
-if __name__ == '__main__':
-    app.run()
--- src/serverlist/tasks.py
+++ src/serverlist/tasks.py
@@ -3,7 +3,11 @@ All celery tasks
 This is mainly for pinging servers to determine their availability
 """
 
-from . import celery
+from celery import Celery
+
+
+# autofinalize will raise a warning if this is initialized before the app is
+celery = Celery(__name__, autofinalize=False)
 
 
 @celery.task
