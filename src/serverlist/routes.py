6b72d10fe54ea0f4579c4c444c106d25592f4d2f
Dylan Tonks <deejtonks@gmail.com>
2019-04-09 00:06:54 +1200

    test graph

--- src/serverlist/routes.py
+++ src/serverlist/routes.py
@@ -4,6 +4,7 @@ For all the views which route to some URL
 
 from flask import Blueprint, current_app, jsonify, render_template
 from sqlalchemy import desc
+import time
 
 from .models import Server, ServerActivity
 
@@ -20,7 +21,7 @@ def server_list():
 def server_activity(id):
 
     server_activity = ServerActivity.query.filter_by(server_id=id)
-    server_activity_list = [[r.timestamp, r.status] for r in server_activity]
+    server_activity_list = [[r.timestamp.strftime("%Y-%m-%d %H:%M:%S"), r.status] for r in server_activity]
     activity = {
         'interval_s': 30 * 60, # 30 minutes
         'data': server_activity_list
